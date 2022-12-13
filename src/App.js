764ce6684457bbf2bd5d8ce762a27c635b896d37
Harsh Rana <rharsh183@gmail.com>
2022-12-13 13:57:04 +0000

    new commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-
-import { useState, useEffect } from "react";
-import List from "./List";
-
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("./noble.json")
+      .then((res) => setUsers(res.data))
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
-    <div className="App">
-      <List />
+    <div>
+      <ul>
+        {users.map((user, index) => (
+          <li key={index}>
+            #{user.laureates.id}: {user.laureates.first_name} 
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
